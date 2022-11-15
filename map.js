03ad3e252e2c71e616f4bc88b941a7c64955de89
SuperProCoolName <minzatov.2004@mail.ru>
2022-11-15 17:26:08 +0300

    idk

--- map.js
+++ map.js
@@ -1,4 +1,30 @@
-var map = L.map('map').setView([44.6116, 33.4921], 5)
+var map = L.map('map', {
+center: [44.6116, 33.4921],
+zoom: 5,
+layers: cities
+});
+
+/*var popup_latlng = L.popup();
+function onMapClick(e) {
+    popup_latlng
+                .setLatLng(e.latlng)
+                .setContent(e.latlng.toString())
+                .openOn(map);
+}
+map.on('click', onMapClick);*/
+
+
+var chersonesus = L.marker([44.6116, 33.4921]).bindPopup('This is Chersonesus');
+var simferopol = L.marker([44.952195, 34.102378]).bindPopup('This is Simferopol');
+var kerch = L.marker([45.354317, 36.453323]).bindPopup('This is Kerch');
+var cities = L.layerGroup([chersonesus, simferopol, kerch]).addTo(map);
+
+var overlayMaps = {
+  "Cities": cities
+};
+
+var layerControl = L.control.layers(null, overlayMaps).addTo(map);
+
 
 map.attributionControl.setPrefix('&copy <a href="https://leafletjs.com" target="_blank">Leaflet</a>')
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,14 +32,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> | &copy; <a href="https://pt.2035.university/project/natures-handbook" target="_blank">GeoChemical Map</a>'
 }).addTo(map);
 
+var scale = L.control.scale();
+scale.addTo(map);
+
 var myLayer = L.geoJSON().addTo(map);
   // load GeoJSON from an external file
   fetch("geo.json").then(res => res.json()).then(data => {
     map_data = L.geoJson(data);
     map_data.addTo(map);
-  });
-
-  
-var scale = L.control.scale();
-scale.addTo(map);
+});
 
